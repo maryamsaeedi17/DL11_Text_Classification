f142ffc1793d5ea258e002f6f490194ae2b465cc
maryamsaeedi17 <m.saeedi64@gmail.com>
2024-02-07 21:26:11 +0330

    object oriented

---
+++ emoji_text_classifier.py
@@ -0,0 +1,27 @@
+
+
+class EmojiTextClassifier():
+    def __init__():
+        ...
+
+    def load_dataset(self):
+        ...
+
+    def loade_feature_vectors(self):
+        ...
+
+    def sentence_to_feature_vectors_avg(self):
+        ...
+
+    def load_model(self):
+        ...
+
+    def train(self):
+        ...
+
+    def test(self):
+        ...
+
+    def predict(self):
+        ...
+
